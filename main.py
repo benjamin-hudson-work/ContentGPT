99f24020483446b3ddc8446641b2ea0f6abee493
benjamin-hudson-work <60156352+benjamin-hudson-work@users.noreply.github.com>
2023-07-10 15:42:58 -0500

    Learning Streaamlit

--- main.py
+++ main.py
@@ -2,10 +2,21 @@ import streamlit as st
 import pinecone
 import json
 import openai
+import os
+import pandas as pd
 
 st.title("ContentGPT")
+source = st.sidebar.markdown("[![Click!](./app/static/git.png)](https://github.com/benjamin-hudson-work/ContentGPT)")
 
 "Welcome! This webapp utilizes ChatGPT to interpret a page for a item on Walmart's website and improve its content for search algorithms!"
 
 "First, input the URL for the item on Walmart's online store, then select which aspect you would like this to improve, then optionally select priority keywords."
+ 
+url = st.text_input("Item page url")
+goal = st.radio("Goal: ", ["Optimize Title", "Optimize Features", "Optimize All Content"])
+keywords_input = st.text_input("Which keywords would you like ChatGPT to emphasize? (Unfinished Feature)")
 
+start = st.button("Start!")
+if start:
+    st.write("Done! (not)") #Execute code here?
+    [url, goal, keywords_input]
