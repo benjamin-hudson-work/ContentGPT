447b165de560f3329c5e2fe586d7fb6fb4c289b4
benjamin-hudson-work <60156352+benjamin-hudson-work@users.noreply.github.com>
2023-07-12 22:47:24 -0500

    Struggling with website scraping

--- main.py
+++ main.py
@@ -4,12 +4,17 @@ import json
 import openai
 import os
 import pandas as pd
+from urllib.parse import urlparse
+import requests
+from bs4 import BeautifulSoup
 
 with open('OpenAI API Key.txt') as f: #Temp
     contents = f.read()
 openai.api_key = contents  #st.secrets[openAiKey] #only works on deployment
-messages = [ ]#{"role": "system", "content": "You are a intelligent assistant."} ]
+#Get the AI into character
+messages = [{"role": "system", "content": "You are an intelligent assistant. Your focus is on conversion marketing. Answer the question as truthfully as possible."} ]
 
+#Sidebar content: link to Github
 st.title("ContentGPT")
 source = st.sidebar.markdown("[![Click!](./app/static/git.png)](https://github.com/benjamin-hudson-work/ContentGPT)")
 
@@ -23,14 +28,18 @@ keywords_input = st.text_input("Which keywords would you like ChatGPT to emphasi
 
 start = st.button("Start!")
 if start: #Execute code here
-    messages.append(
-        {"role": "user", "content": "Hello, ChatGPT!"},
-    )
-    chat = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo", messages=messages
-    )
-    reply = chat.choices[0].message.content
-    messages.append({"role": "assistant", "content": reply})
-    messages
+    
 
-#phone_number = st.slider("Input phone number for more",max_value=9999999999) 
+    '''
+    if url:
+        path = urlparse(url).path #Shorten link to ease AI's understanding
+        messages.append( 
+            {"role": "user", "content": "Tell me what the name of the product on this page is: " + path + " Then, tell me what would you change the name of the previous product to in order to improve conversion?"},
+        )
+        chat = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo", messages=messages
+        )
+        reply = chat.choices[0].message.content
+        messages.append({"role": "assistant", "content": reply})
+        messages
+    '''
