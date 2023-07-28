255d3ddfe589c71e0f975caff2fbb157c4f5496d
benjamin-hudson-work <60156352+benjamin-hudson-work@users.noreply.github.com>
2023-07-28 00:42:38 -0500

    bug fix

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ EMODEL = "text-embedding-ada-002"
 openai.api_key = st.secrets['OPENAI_KEY'] 
 pApiKey = st.secrets['PINECONE_KEY'] 
 ac="text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
-headers={"Referer":"https://www.google.com","Connection":"Keep-Alive","Accept-Language":"en-US,en;q=0.9","Accept-Encoding":"gzip, deflate, br","Accept":ac,"User-Agent":"Mozilla/5.0 (iPad; CPU OS 9_3_5 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13G36 Safari/601.1"}
+headers={"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36"}
 
 #Pinecone Block#
 
@@ -42,8 +42,6 @@ index = pinecone.Index('conversion')
 
 def scrape(url, target): #Inputs are url of Walmart store page and the type of data requested.
     resp = requests.get(url, headers=headers)
-    if("Robot or human" in resp.text):
-        return "False"
     soup = BeautifulSoup(resp.text,'html.parser')
     l=[]
     obj={}
